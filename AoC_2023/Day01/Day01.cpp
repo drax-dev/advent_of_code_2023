22d2f69a98e19f5718a292b4a5612fbecab4d176
Pawel Morawski <morawski.pawel@outlook.com>
2023-12-02 01:19:32 +0100

    add solution for day01 part 2

--- AoC_2023/Day01/Day01.cpp
+++ AoC_2023/Day01/Day01.cpp
@@ -4,8 +4,13 @@
 #include "Utils.h"
 #include <vector>
 #include <string>
+#include <unordered_map>
 
-std::vector<int> processData(const std::vector<std::string>& splitLines)
+std::unordered_map<std::string, int> mapTextToNumber{{"one", 1}, {"two", 2}, {"three", 3}, {"four", 4},
+	{"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}, {"zero", 0} };
+
+
+std::vector<int> processDataPartOne(const std::vector<std::string>& splitLines)
 {
 	std::vector<int> results;
 	int firstDigit = -1;
@@ -20,13 +25,14 @@ std::vector<int> processData(const std::vector<std::string>& splitLines)
 			{
 				firstDigit = Utils::strToInt(charFromLeft);
 			}
+
 			if (const auto charFromRight = line[lineLength - 1 - i];
 				secondDigit == -1 && isdigit(charFromRight))
 			{
 				secondDigit = Utils::strToInt(charFromRight);
 			}
 
-			if(firstDigit != -1 && secondDigit != -1)
+			if (firstDigit != -1 && secondDigit != -1)
 			{
 				results.emplace_back(firstDigit * 10 + secondDigit);
 				firstDigit = -1;
@@ -38,12 +44,100 @@ std::vector<int> processData(const std::vector<std::string>& splitLines)
 	return results;
 }
 
+std::vector<int> getAllDigitsFromText(const std::string& text)
+{
+	std::vector<int> results;
+	const auto textLength = text.length();
+	std::string temp;
+	for (std::size_t i = 0; i < textLength; ++i)
+	{
+		temp += text[i];
+		for (std::size_t j = i + 1; j < textLength; ++j)
+		{
+	
+			temp += text[j];
+			if (mapTextToNumber.contains(temp) || temp.length() >= 5)
+			{
+				break;
+			}
+		}
+
+		if (mapTextToNumber.contains(temp))
+		{
+			results.emplace_back(mapTextToNumber[temp]);
+		}
+		temp.clear();
+	}
+	return results;
+}
+
+std::vector<int> processDataPartTwo(const std::vector<std::string>& splitLines)
+{
+	std::vector<int> results;
+	std::vector<int> digits;
+	std::string text;
+	for (const auto& line : splitLines)
+	{
+		digits.clear();
+		text.clear();
+		const auto lineLength = line.length();
+		for (std::size_t i = 0; i < lineLength; ++i)
+		{
+			if (const auto singleChar = line[i];
+				isdigit(singleChar))
+			{
+				if (!text.empty())
+				{
+					if (mapTextToNumber.contains(text))
+					{
+						digits.emplace_back(mapTextToNumber[text]);
+					}
+					else
+					{
+						const auto digitsFromLetters = getAllDigitsFromText(text);
+						digits.insert(digits.end(), digitsFromLetters.begin(), digitsFromLetters.end());
+					}
+				}
+				digits.emplace_back(Utils::strToInt(singleChar));
+				text.clear();
+			}
+			else
+			{
+				text += singleChar;
+			}
+		}
+		if (!text.empty())
+		{
+			const auto digitsFromLetters = getAllDigitsFromText(text);
+			digits.insert(digits.end(), digitsFromLetters.begin(), digitsFromLetters.end());
+		}
+
+		if (!digits.empty())
+		{
+			results.emplace_back(digits.front() * 10 + digits.back());
+		}
+		else
+		{
+			throw std::runtime_error("digits vector should never be empty");
+		}
+	}
+	return results;
+}
+
 int main()
 {
+
 	// part 1
 	const auto data = Utils::read_file("day01_input.txt");
 	const auto splitLines = Utils::split_string(data);
-	const auto results = processData(splitLines);
+	const auto results = processDataPartOne(splitLines);
 	const auto sum = std::accumulate(results.begin(), results.end(), 0);
 	std::cout << "result is: " << sum << std::endl;
+
+	// part 2
+	const auto dataPartTwo = Utils::read_file("day01_input.txt");
+	const auto splitLinesPartTwo = Utils::split_string(dataPartTwo);
+	const auto resultsPartTwo = processDataPartTwo(splitLinesPartTwo);
+	const auto sumPartTwo = std::accumulate(resultsPartTwo.begin(), resultsPartTwo.end(), 0);
+	std::cout << "result is: " << sumPartTwo << std::endl;
 }

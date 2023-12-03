831683e1ae5fa5bdcb41082eda1c248e87196658
Pawel Morawski <morawski.pawel@outlook.com>
2023-12-03 20:32:49 +0100

    add solution for day03 part2

--- AoC_2023/Day03/Day03.cpp
+++ AoC_2023/Day03/Day03.cpp
@@ -5,13 +5,13 @@
 
 struct GridNode
 {
-	GridNode() : x(0), y(0), value('.'), isPartNumber(false)
+	GridNode() : x(0), y(0), value('.'), isPartNumber(false), isGear(false)
 	{
 
 	}
 
 	GridNode(const std::size_t inputX, const std::size_t inputY, const char inputValue = '.')
-		: x(inputX), y(inputY), value(inputValue), isPartNumber(false)
+		: x(inputX), y(inputY), value(inputValue), isPartNumber(false), isGear(false)
 	{
 
 	}
@@ -21,6 +21,7 @@ struct GridNode
 
 	char value;
 	bool isPartNumber;
+	bool isGear;
 };
 
 struct Grid
@@ -182,7 +183,7 @@ Grid initGrid(const std::vector<std::string>& splitLines)
 	return grid;
 }
 
-void setPartNumberFlagForAllNodes(Grid& grid)
+void setPartNumberFlagForAllValidNodes(Grid& grid)
 {
 	for (auto& nodesRow : grid.gridNodes)
 	{
@@ -190,7 +191,7 @@ void setPartNumberFlagForAllNodes(Grid& grid)
 		{
 			if (isdigit(node->value))
 			{
-				if(grid.isNodeAdjacentToSymbol(node))
+				if (grid.isNodeAdjacentToSymbol(node))
 				{
 					node->isPartNumber = true;
 				}
@@ -245,14 +246,170 @@ std::vector<int> processDataPart1(const Grid& grid)
 	return results;
 }
 
+void setIsGridFlagForAllValidNodes(Grid& grid)
+{
+	const auto ySize = grid.gridYSize;
+	const auto xSize = grid.gridXSize;
+	for (std::size_t y = 0; y < ySize; ++y)
+	{
+		for (std::size_t x = 0; x < xSize; ++x)
+		{
+			if (const auto node = grid.gridNodes[y][x];
+				node->value == '*')
+			{
+				const auto neighbours = grid.getAllNeighbours(node);
+
+				std::size_t partNumberAmount = 0;
+				for (std::size_t i = 0; i < 8; ++i)
+				{
+					const auto& neighbour = neighbours[i];
+					if (i == 3 || i == 7)
+					{
+						if (isdigit(neighbour->value))
+						{
+							if (!isdigit(neighbours[i - 3]->value))
+							{
+								partNumberAmount++;
+							}
+						}
+					}
+					else if (i == 4 || i == 5)
+					{
+						if (isdigit(neighbour->value))
+						{
+							if (!isdigit(neighbours[i - 2]->value))
+							{
+								partNumberAmount++;
+							}
+						}
+					}
+					else
+					{
+						if (isdigit(neighbour->value))
+						{
+							partNumberAmount++;
+						}
+					}
+				}
+				if (partNumberAmount == 2)
+				{
+					node->isGear = true;
+				}
+			}
+		}
+	}
+}
+
+int getWholePartNumber(Grid& grid, const std::shared_ptr<GridNode>& gridNode)
+{
+	std::string temp{ gridNode->value };
+	std::shared_ptr<GridNode> predecessor = gridNode;
+	while (predecessor != nullptr)
+	{
+		predecessor = grid.getLeftMiddleNeighbour(predecessor->x, predecessor->y);
+		if (predecessor != nullptr && isdigit(predecessor->value))
+		{
+			temp.insert( 0, std::string{predecessor->value});
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	std::shared_ptr<GridNode> successor = gridNode;
+	while (successor != nullptr && isdigit(successor->value))
+	{
+		successor = grid.getRightMiddleNeighbour(successor->x, successor->y);
+		if (successor != nullptr && isdigit(successor->value))
+		{
+			temp += successor->value;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return Utils::strToInt(temp);
+}
+
+std::vector<int> processDataPart2(Grid& grid)
+{
+	std::vector<int> results;
+	const auto ySize = grid.gridYSize;
+	const auto xSize = grid.gridXSize;
+	for (std::size_t y = 0; y < ySize; ++y)
+	{
+		for (std::size_t x = 0; x < xSize; ++x)
+		{
+			if (const auto node = grid.gridNodes[y][x];
+				node->isGear)
+			{
+				const auto neighbours = grid.getAllNeighbours(node);
+				std::vector<int> gearRatios;
+				for (std::size_t i = 0; i < 8; ++i)
+				{
+					const auto& neighbour = neighbours[i];
+					if (i == 3 || i == 7)
+					{
+						if (isdigit(neighbour->value))
+						{
+							if (!isdigit(neighbours[i - 3]->value))
+							{
+								gearRatios.emplace_back(getWholePartNumber(grid, neighbour));
+							}
+						}
+					}
+					else if (i == 4 || i == 5)
+					{
+						if (isdigit(neighbour->value))
+						{
+							if (!isdigit(neighbours[i - 2]->value))
+							{
+								gearRatios.emplace_back(getWholePartNumber(grid, neighbour));
+							}
+						}
+					}
+					else
+					{
+						if (isdigit(neighbour->value))
+						{
+							gearRatios.emplace_back(getWholePartNumber(grid, neighbour));
+						}
+					}
+				}
+				if (gearRatios.size() == 2)
+				{
+					results.emplace_back(gearRatios.front() * gearRatios.back());
+				}
+				else
+				{
+					throw std::runtime_error("Wrong number of gear ratios for gear at pos: [" + std::to_string(x) + ", " + std::to_string(y) + ']');
+				}
+			}
+		}
+	}
+	return results;
+}
+
+
 int main()
 {
 	// part 1
 	const auto data = Utils::read_file("day03_input.txt");
 	const auto splitLines = Utils::split_string(data);
 	auto grid = initGrid(splitLines);
-	setPartNumberFlagForAllNodes(grid);
+	setPartNumberFlagForAllValidNodes(grid);
 	const auto results = processDataPart1(grid);
 	const auto sum = std::accumulate(results.begin(), results.end(), 0);
 	std::cout << "sum of all the part numbers is " << sum << std::endl;
+
+	// part 2
+	const auto dataPart2 = Utils::read_file("day03_input.txt");
+	const auto splitLinesPart2 = Utils::split_string(dataPart2);
+	auto gridPart2 = initGrid(splitLinesPart2);
+	setIsGridFlagForAllValidNodes(gridPart2);
+	const auto resultsPart2 = processDataPart2(gridPart2);
+	const auto sumPart2 = std::accumulate(resultsPart2.begin(), resultsPart2.end(), 0);
+	std::cout << "sum of all gear ratios is " << sumPart2 << std::endl;
 }

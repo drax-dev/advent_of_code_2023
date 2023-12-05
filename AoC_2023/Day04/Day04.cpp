985b56a21647f1555499a979d66888065e126343
Pawel Morawski <morawski.pawel@outlook.com>
2023-12-05 16:04:02 +0100

    add solution for day04 part2

--- AoC_2023/Day04/Day04.cpp
+++ AoC_2023/Day04/Day04.cpp
@@ -1,5 +1,6 @@
 #include "Utils.h"
 #include <iostream>
+#include <numeric>
 #include <set>
 
 std::set<int> parseNumbers(const std::string& text)
@@ -69,6 +70,53 @@ unsigned long long processDataPart1(const std::vector<std::string>& splitLines)
 	return result;
 }
 
+unsigned long long processDataPart2(const std::vector<std::string>& splitLines)
+{
+	unsigned long long result = 0;
+	std::vector multipliers(splitLines.size(), 1);
+	int lineIndex = 0;
+	for (const auto& line : splitLines)
+	{
+		if (auto temp = Utils::split_string(line, ':');
+			temp.size() == 2)
+		{
+			temp = Utils::split_string(temp.at(1), '|');
+			if (temp.size() == 2)
+			{
+				const auto winningNumbers = parseNumbers(temp[0]);
+				const auto cardNumbers = parseNumbers(temp[1]);
+				int points = 0;
+				for (const int winningNumber : winningNumbers)
+				{
+					for (const int cardNumber : cardNumbers)
+					{
+						if (cardNumber == winningNumber)
+						{
+							points++;
+						}
+					}
+				}
+				const auto multiplier = multipliers[lineIndex];
+				for(int counter =1; counter <= points; ++counter)
+				{
+					multipliers[lineIndex + counter] += multiplier;
+				}
+			}
+			else
+			{
+				throw std::runtime_error("Parser failed on parsing \'|\'");
+			}
+		}
+		else
+		{
+			throw std::runtime_error("Parser failed on parsing \':\'");
+		}
+		lineIndex++;
+	}
+	result = std::accumulate(multipliers.begin(), multipliers.end(), 0);
+	return result;
+}
+
 int main()
 {
 	// part 1
@@ -77,4 +125,9 @@ int main()
 	const auto result = processDataPart1(splitLines);
 	std::cout << "Total number of points is " << result << std::endl;
 
+	// part 2
+	const auto dataPart2 = Utils::read_file("day04_input.txt");
+	const auto splitLinesPart2 = Utils::split_string(dataPart2);
+	const auto resultPart2 = processDataPart2(splitLinesPart2);
+	std::cout << "Total number of scratchcards is " << resultPart2 << std::endl;
 }
